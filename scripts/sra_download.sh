31df6032009e46c3e333e4a3c46ea16510b1b55c
Sreemol Gokuladhas sreemol.gokuladhas@auckland.ac.nz <sgok603@mahuika01.mahuika.nesi.org.nz>
2022-05-22 13:00:27 +1200

    edit threads

--- scripts/sra_download.sh
+++ scripts/sra_download.sh
@@ -3,13 +3,13 @@
 module load parallel-fastq-dump/0.6.6-gimkl-2020a-Python-3.8.2
 
 input="/scale_wlg_persistent/filesets/project/uoa02869/colon_cancer_hic/raw_data/srr_ids.txt"
-tmp_dir="/scale_wlg_persistent/filesets/project/uoa02869/colon_cancer_hic/scripts"
+tmp_dir="/scale_wlg_persistent/filesets/project/uoa02869/colon_cancer_hic/raw_data/fastq"
 out_dir="/scale_wlg_persistent/filesets/project/uoa02869/colon_cancer_hic/raw_data/fastq"
 
 cat $input | while read line || [[ -n $line ]];
 do
     echo $line "downloading..."
-    parallel-fastq-dump --sra-id $line --threads 32 --tmpdir $tmp_dir --outdir $out_dir --split-files --gzip
+    parallel-fastq-dump --sra-id $line --threads 48 --tmpdir $tmp_dir --outdir $out_dir --split-files --gzip
     echo $line "done."
 done
 
